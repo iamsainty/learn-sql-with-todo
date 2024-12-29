c39f0248333b249292aa44e7305264afac489509
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2024-12-29 14:32:14 +0530

    Set Up Your Environment

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(bodyParser.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Welcome to the SQL To-Do App');
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
